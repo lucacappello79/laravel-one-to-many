a74cec8f3546356380debc21d3eb9371f3830b4e
lucacappello79 <postt79@gmail.com>
2023-05-23 16:54:27 +0200

    feat: adding type seeder, updated DBseeder

--- database/migrations/2023_05_23_140835_create_types_table.php
+++ database/migrations/2023_05_23_140835_create_types_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->id();
 
-
-
-
+            $table->string('name', 100)->unique();
+            $table->string('slug', 100);
+            $table->string('description')->nullable();
 
             $table->timestamps();
         });
