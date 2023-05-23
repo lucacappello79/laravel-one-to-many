34ad16f512e2e9e0b52c98343168f782f9f24ad5
lucacappello79 <postt79@gmail.com>
2023-05-23 16:04:10 +0200

    feat: updating models

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function home()
     {
+        // dd(Auth::user());
+        // dd(Auth::id());
+        $user = Auth::user();
+        dd($user->userDetail?->address);
+
         return view('admin.dashboard');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userDetail()
+    {
+
+        return $this->hasOne(UserDetail::class);
+    }
 }
